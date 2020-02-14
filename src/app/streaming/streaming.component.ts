546d33fb814c7b0de20e32ef87fd5ba563826170
Tim Finnigan <tim.finnigan1@gmail.com>
2020-02-14 09:52:10 -0500

    updating colors

--- src/app/streaming/streaming.component.ts
+++ src/app/streaming/streaming.component.ts
@@ -22,13 +22,13 @@ export class StreamingComponent implements OnInit {
         datasets: [{
           label: 'Subscribers',
           data: [150000000, 100000000, 28000000, 8000000, 3000000],
-          // backgroundColor: [
-          //   'red',
-          //   'blue',
-          //   'green',
-          //   'black',
-          //   'gold'
-          // ],
+          backgroundColor: [
+            'rgba(229, 9, 20, .7)',
+            'rgba(255, 153, 0, .7)',
+            'rgba(102, 170, 50, .7)',
+            'rgba(0, 0, 0, .7)',
+            'rgba(192,192,192, .7)'
+          ],
           borderWidth: 1
         }]
       },
