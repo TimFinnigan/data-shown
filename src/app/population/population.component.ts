f4afebcb07ce5f6620fbaa222926655985573d20
Tim Finnigan <tim.finnigan1@gmail.com>
2020-04-28 14:27:36 -0400

    update population route with new chart

--- src/app/population/population.component.ts
+++ src/app/population/population.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as Chart from 'chart.js'
 @Component({
-  selector: 'app-population',
+  selector: '',
   templateUrl: './population.component.html',
   styleUrls: ['./population.component.css']
 })
@@ -9,7 +9,47 @@ export class PopulationComponent implements OnInit {
 
   constructor() { }
 
+  canvas: any;
+  ctx: any;
   ngOnInit() {
+    new Chart(document.getElementById("cases-by-country"), {
+      type: 'horizontalBar',
+      data: {
+        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+        datasets: [
+          {
+            label: "Population (millions)",
+            backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+            data: [2478, 5267, 734, 784, 433]
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        display: true,
+        title: {
+          display: false,
+          text: 'Population'
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          mode: 'index'
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+            },
+            scaleLabel: {
+              display: false,
+              labelString: 'Countries'
+            }
+          }]
+        }
+      }
+    });
   }
-
 }
