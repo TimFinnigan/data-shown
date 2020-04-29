7615547ec15574bd2e0d632d3ebe2a726dba7f50
Tim Finnigan <tim.finnigan1@gmail.com>
2020-04-29 08:21:29 -0400

    add datalabels

--- src/app/population/population.component.ts
+++ src/app/population/population.component.ts
@@ -31,8 +31,7 @@ export class PopulationComponent implements OnInit {
         ]
       },
       options: {
-      fillColor: "rgba(0,60,100,1)",
-
+        fillColor: "rgba(0,60,100,1)",
         responsive: true,
         maintainAspectRatio: false,
         display: true,
@@ -62,7 +61,26 @@ export class PopulationComponent implements OnInit {
               }
             }]
         },
-
+        hover: {
+          animationDuration: 0
+        },
+        animation: {
+          duration: 1000,
+          onComplete: function () {
+            var chartInstance = this.chart,
+              ctx = chartInstance.ctx;
+            ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'bottom';
+            this.data.datasets.forEach(function (dataset, i) {
+              var meta = chartInstance.controller.getDatasetMeta(i);
+              meta.data.forEach(function (bar, index) {
+                var data = dataset.data[index] + " B";
+                ctx.fillText(data, bar._model.x + 20, bar._model.y + 5);
+              });
+            });
+          }
+        },
       }
     });
   }
