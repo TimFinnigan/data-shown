dc1466c692a4619b95910cd74238165cc9cd1243
Tim Finnigan <tim.finnigan1@gmail.com>
2020-05-03 16:13:33 -0400

    get average duration

--- src/app/youtube/youtube.component.ts
+++ src/app/youtube/youtube.component.ts
@@ -13,26 +13,25 @@ export class YoutubeComponent implements OnInit {
   videoData: any = Object.values(youtubeData["default"]);
 
   constructor() {
-    console.log(this.videoData);
   }
 
   ngOnInit() {
-    // Durations format example: "PT2M31S"
+    // Durations format example in JSON: "PT2M31S"
     let videos = this.videoData;
+    let totalTimeInMinutes = 0;
     for (let i = 0; i < videos.length; i++) {
       let duration = videos[i].duration;
-
-      console.log(duration);
-
       duration = duration.split("M");
-
-      console.log(duration);
-
       let minutes = duration[0].replace(/\D/g, "");
       let seconds = duration[1].replace(/\D/g, "");
-      console.log(minutes);
-      console.log(seconds);
+      let timeInMinutes = parseInt(minutes);
+      if (seconds) {
+        timeInMinutes += (parseInt(seconds) / 60);
+      }
+      totalTimeInMinutes += timeInMinutes;
     }
+    let averageMinutes = totalTimeInMinutes / 50;
+    console.log(averageMinutes.toFixed(2));
     console.group(this.videoData);
     new Chart(document.getElementById("cases-by-country"), loadYoutubeChart(youtubeData, youtubeCategories));
   }
